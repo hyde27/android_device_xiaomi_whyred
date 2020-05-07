1f53216a95ceefe6ff9df08877dc29ad85577e4e
Jose Enrique <52612985+hyde27@users.noreply.github.com>
2020-05-07 11:18:04 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_whyred.mk
+    $(LOCAL_DIR)/xtended_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_whyred-user \
-    lineage_whyred-userdebug \
-    lineage_whyred-eng
+    xtended_whyred-user \
+    xtended_whyred-userdebug \
+    xtended_whyred-eng
